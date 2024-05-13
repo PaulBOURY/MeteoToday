1c4d9b9dbd486cd3125389eca83ec54658201f73
PAUL BOURY <paulboury17@gmail.com>
2024-05-13 14:32:04 +0200

    Added a second fetch to get all the data we need

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,30 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 export default function Home() {
+  const [weatherMax, setWeatherMax] = useState([])
+  const [weatherMin, setWeatherMin] = useState([])
   useEffect(()=>{
-    fetch("https://api.open-meteo.com/v1/forecast?latitude=44.8404&longitude=-0.5805&hourly=temperature_2m,rain")
+    fetch("https://api.open-meteo.com/v1/meteofrance?latitude=44.8404&longitude=-0.5805&daily=temperature_2m_max,temperature_2m_min&timezone=Europe%2FLondon")
       .then (response=>response.json())
-      .then(data=>console.log(data))
-      .catch((error)=>{console.error("il y a une erreur c'est pas d'chance",error)})
+      .then(data=> setWeatherMax(data.daily.temperature_2m_max))
+      .catch((error)=>{console.error("il y a une erreur c'est pas d'chance",error)});
+    fetch("https://api.open-meteo.com/v1/meteofrance?latitude=44.8404&longitude=-0.5805&daily=temperature_2m_max,temperature_2m_min&timezone=Europe%2FLondon")
+      .then (response=>response.json())
+      .then(data=> setWeatherMin(data.daily.temperature_2m_min))
+      .catch((error)=>{console.error("il y a une erreur c'est pas d'chance",error)});
   },[])
+  console.log(weatherMax)
+  console.log(weatherMin)
   return (
-    <div>
-      <p></p>
-    </div>
+    <>
+    <ul>
+      {Object.entries(weatherMax).map((value, index) => (<li key={index}> Max {value[1]}
+      </li>))}
+    </ul>
+    <ul>
+      {Object.entries(weatherMin).map((value, index) => (<li key={index}> Min {value[1]}
+      </li>))}
+    </ul>
+    </>
   )
 }
