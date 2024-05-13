7114e94894d54611f40e2d0a18eba3929916dcbe
Rollet Fabien <fabrol47@gmail.com>
2024-05-13 11:01:01 +0200

    fetch + chart.js

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,15 @@
+import { useEffect } from "react"
+
 export default function Home() {
+  useEffect(()=>{
+    fetch("https://api.open-meteo.com/v1/forecast?latitude=44.8404&longitude=-0.5805&hourly=temperature_2m,rain")
+      .then (response=>response.json())
+      .then(data=>console.log(data))
+      .catch((error)=>{console.error("il y a une erreur c'est pas d'chance",error)})
+  },[])
   return (
     <div>
-      <p>Salut</p>
+      <p></p>
     </div>
   )
 }
